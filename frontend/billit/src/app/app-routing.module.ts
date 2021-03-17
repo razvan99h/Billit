3e5af33cde16be2b60bc8aee7b78249c697a8e0d
Razvan Horhat <razvan.horhat@gitlab.com>
2021-03-17 09:59:03 +0200

    Added login page (WIP)

--- frontend/billit/src/app/app-routing.module.ts
+++ frontend/billit/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // TODO: make this the main endpoint after a user is logged in
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
